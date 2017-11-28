6c8a2f6c928b8395c0417f844a4f387c5977ad54
Jiahuan He <jiahuan_he@outlook.com>
2017-11-27 22:00:30 -0600

    Set up table view

--- ios/where-to-go/ViewController.swift
+++ ios/where-to-go/ViewController.swift
@@ -11,13 +11,6 @@ import GooglePlaces
 import CoreLocation
 
 
-//
-//func isLocationPermissionGranted() -> Bool
-//{
-//    guard CLLocationManager.locationServicesEnabled() else { return false }
-//    return [.authorizedAlways, .authorizedWhenInUse].contains(CLLocationManager.authorizationStatus())
-//}
-
 func isLocationAllowed() -> Bool{
     if CLLocationManager.locationServicesEnabled() {
         switch(CLLocationManager.authorizationStatus()) {
@@ -34,57 +27,63 @@ func isLocationAllowed() -> Bool{
     }
 }
 
-class ViewController: UIViewController, GMSAutocompleteViewControllerDelegate{
+class ViewController: UIViewController, UITableViewDelegate,  UITableViewDataSource, GMSAutocompleteViewControllerDelegate{
     
-    var placesClient: GMSPlacesClient!
-    @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var addressLabel: UILabel!
+//    var placesClient: GMSPlacesClient!
     let localisationManager = CLLocationManager()
+    var placeIDs = [String]()
     
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidAppear(_ animated: Bool) {
+        tableView.frame = CGRect(x: tableView.frame.origin.x, y: tableView.frame.origin.y, width: tableView.frame.size.width, height: tableView.contentSize.height)
+    }
+    
+    override func viewDidLayoutSubviews(){
+        tableView.frame = CGRect(x: tableView.frame.origin.x, y: tableView.frame.origin.y, width: tableView.frame.size.width, height: tableView.contentSize.height)
+        tableView.reloadData()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //        placesClient = GMSPlacesClient.shared()
+        if !isLocationAllowed() {
+            localisationManager.requestWhenInUseAuthorization()
+        }
+        tableView.delegate = self
+        tableView.dataSource = self
+        // temp:
+//        placeIDs.append("place1")
+//        placeIDs.append("place2")
+//        placeIDs.append("place3")
+//        placeIDs.append("place4")
+    }
+//////////// START => Tableview data source ////////////
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = placeIDs[indexPath.row]
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return placeIDs.count
+    }
+    
+    
+//////////// END => Tableview data source ////////////
+
+//////////// START => Google Places Controller delegate ////////////
     //Present the Autocomplete view controller
     @IBAction func autocompleteClicked(_ sender: UIButton) {
         let autocompleteController = GMSAutocompleteViewController()
         autocompleteController.delegate = self
         present(autocompleteController, animated: true, completion: nil)
     }
-  
-    func requestAuthorization() {
-        localisationManager.requestWhenInUseAuthorization()
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        placesClient = GMSPlacesClient.shared()
-        if !isLocationAllowed() {
-            requestAuthorization()
-        }
-    }
-    
-    @IBAction func getCurrentPlace(_ sender: UIButton) {
-        // For getting the user permission to use location service when the app is running
-        placesClient.currentPlace(callback: { (placeLikelihoodList, error) -> Void in
-            if let error = error {
-                print("Pick Place error: \(error.localizedDescription)")
-                return
-            }
-            
-            self.nameLabel.text = "No current place"
-            self.addressLabel.text = ""
-            
-            if let placeLikelihoodList = placeLikelihoodList {
-                let place = placeLikelihoodList.likelihoods.first?.place
-                if let place = place {
-                    self.nameLabel.text = place.name
-                    self.addressLabel.text = place.formattedAddress?.components(separatedBy: ", ")
-                        .joined(separator: "\n")
-                }
-            }
-        })
-    }
-    
-    
-    
     
     // Turn the network activity indicator on and off again.
     func didRequestAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
@@ -96,6 +95,9 @@ class ViewController: UIViewController, GMSAutocompleteViewControllerDelegate{
     }
     
     func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+        print("Place attributions: \(place.placeID)")
+        placeIDs.append(place.name )
+        tableView.reloadData()
         dismiss(animated: true, completion: nil)
     }
     
@@ -107,40 +109,9 @@ class ViewController: UIViewController, GMSAutocompleteViewControllerDelegate{
     func wasCancelled(_ viewController: GMSAutocompleteViewController) {
         dismiss(animated: true, completion: nil)
     }
-    
+////////////    END => Google Places Controller delegate ////////////
     
 }
-//
-//extension ViewController: GMSAutocompleteViewControllerDelegate {
-//    
-//    // Handle the user's selection.
-//    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
-//        print("Place name: \(place.name)")
-//        print("Place address: \(place.formattedAddress)")
-//        print("Place attributions: \(place.attributions)")
-//        dismiss(animated: true, completion: nil)
-//    }
-//    
-//    func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
-//        // TODO: handle the error.
-//        print("Error: ", error.localizedDescription)
-//    }
-//    
-//    // User canceled the operation.
-//    func wasCancelled(_ viewController: GMSAutocompleteViewController) {
-//        dismiss(animated: true, completion: nil)
-//    }
-//    
-//    // Turn the network activity indicator on and off again.
-//    func didRequestAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
-//        UIApplication.shared.isNetworkActivityIndicatorVisible = true
-//    }
-//    
-//    func didUpdateAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
-//        UIApplication.shared.isNetworkActivityIndicatorVisible = false
-//    }
-//    
-//}
 
 
 
